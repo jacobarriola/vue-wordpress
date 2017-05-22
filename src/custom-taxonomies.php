a52f76db2cfbb8b5f8bc2c78d29339accf9f7919
Jacob Arriola <jacob@zeek.com>
2017-05-21 21:23:01 -0700

    Add gender taxonomy

--- src/custom-taxonomies.php
+++ src/custom-taxonomies.php
@@ -16,4 +16,18 @@ add_action( 'init', function() {
 		"show_in_quick_edit" => true,
 	];
 	register_taxonomy( get_taxonomy_superhero_category(), get_type_superhero(), $args );
+
+	$labels = [
+		"name"          => esc_html( 'Gender' ),
+		"singular_name" => esc_html( 'Gender' ),
+	];
+	$args = [
+		"labels"             => $labels,
+		"hierarchical"       => true,
+		'public'             => true,
+		"show_admin_column"  => true,
+		"show_in_rest"       => true,
+		"show_in_quick_edit" => true,
+	];
+	register_taxonomy( get_taxonomy_gender(), get_type_superhero(), $args );
 } );
