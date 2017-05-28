dc53910f0aca6729709262aea106fe76ac0a574b
Jacob Arriola <jacob@zeek.com>
2017-05-27 20:35:53 -0700

    🔥 Remove category

--- src/custom-taxonomies.php
+++ src/custom-taxonomies.php
@@ -3,19 +3,6 @@
 namespace Vue_WP;
 
 add_action( 'init', function() {
-	$labels = [
-		"name"          => esc_html( 'Superhero Categories' ),
-		"singular_name" => esc_html( 'Superhero Category' ),
-	];
-	$args = [
-		"labels"             => $labels,
-		"hierarchical"       => true,
-		'public'             => true,
-		"show_admin_column"  => true,
-		"show_in_rest"       => true,
-		"show_in_quick_edit" => true,
-	];
-	register_taxonomy( get_taxonomy_superhero_category(), get_type_superhero(), $args );
 
 	$labels = [
 		"name"          => esc_html( 'Gender' ),
@@ -30,4 +17,5 @@ add_action( 'init', function() {
 		"show_in_quick_edit" => true,
 	];
 	register_taxonomy( get_taxonomy_gender(), get_type_superhero(), $args );
+
 } );
